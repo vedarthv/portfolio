350c6d5a7efa488387f5c0dfe7ded65f2d94eaf5
Vedarth V <vedarth1@gmail.com>
2023-01-23 15:58:04 -0500

    adjusted grid breakpoints

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Chat from "./Chat";
 
 export default function App() {
   return (
-    <div className="grid grid-cols-2 grid-row-2 md:space-x-4 space-y-2  grid-flow-row md:grid-flow-col min-h-screen min-w-screen">
-      <div className="grid col-span-2 row-span-1 md:col-span-1 md:row-span-2 m-1 p-1 card bg-base-300 rounded-box min-h-fit justify-center">
+    <div className="grid grid-cols-2 md:grid-col-1 md:space-x-4 space-y-2   min-h-screen min-w-screen">
+      <div className="grid col-span-2 md:col-span-1 m-1 p-1 card bg-base-300 rounded-box min-h-fit justify-center">
         <Chat />
       </div>
 
